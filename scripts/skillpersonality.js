2b98e690a4266bd2f7d4611e7075ea4533378ccf
jhoyuen <jon_lg83@yahoo.com>
2020-05-06 21:02:41 +1000

    Add files via upload

--- scripts/skillpersonality.js
+++ scripts/skillpersonality.js
@@ -3,10 +3,15 @@ var spells = {
 	meteor: { title: "Meteor", description: "Meteors falling from the sky!", reallife: "\"Solving many issues using a different solution for each issue...\"" },
 	chainlightning: { title: "Chain Lightning", description: "Electric discharge that jumps from the primary target to secondary nearby targets", reallife: "\"Solving a big problem with the intent that other smaller issues get tackled at the same time...\"" },
 	blizzard: { title: "Blizzard", description: "A tempest of hail, snow, rain and thunder", reallife: "\"Slowing down an area of issues by slowing them down...\"" },
-	tornado: { title: "Tornado", description: "", reallife: "" },
-	lightningstrike: { title: "Lightning Strike", description: "", reallife: "" },
-	firebolt: { title: "Fire Bolt", description: "", reallife: "" },
-	thunderbolt: { title: "Thunder Bolt", description: "", reallife: "" },
+	tornado: { title: "Tornado", description: "Strong gusts of wind that damage and delay all in its path", reallife: "\"Come up with a quick fixes in an area of related problems. This is not particularly strong but it delays the problems for some time until a proper solution can be found.\"" },
+	lightningstrike: { title: "Lightning Strike", description: "A single discharge of electricity", reallife: "\"Tackle a single problem\"" },
+	firebolt: { title: "Fire Bolt", description: "A sudden flash of fire", reallife: "\"A quick burst for a fix. Quick, heated but effective\"" },
+	thunderbolt: { title: "Thunder Bolt", description: "A sudden flash of thunder", reallife: "\"A quick burst for a fix. Quick, loud but effective\"" },
+	earthsmash: { title: "Earth Smash", description: "A giant rock smashes the earth", reallife: "\"a fix that packs a punch and is here to stay...\"" },
+	thunderwave: { title: "Thunderwave", description: "A blaze of thunder booming in a line", reallife: "\"Loudly fix all problems that lie in front of you\"" },
+	firewave: { title: "Firewave", description: "A blaze of fire burning in a line", reallife: "\"Passionately fix all problems that lie in front of you\"" },
+	teleport: { title: "Teleport", description: "Magically disapear and appear at a different point of space and time", reallife: "\"Seem to be anywhere, everywhere anytime...\"" },
+	animate: { title: "Animate", description: "Bring a ", reallife: "\"Seem to be anywhere, everywhere anytime...\"" },
 };
 
 function init(){	
@@ -14,7 +19,8 @@ function init(){
 		$('html, body').animate(
         {scrollTop: $('.spell-item').offset().top - 170}, 
         500);
-		
+		console.log('images/' + $(this).attr('id') + '.png');
+		$('.spellimage').css('background', 'url(\'images/' + $(this).attr('id') + '.png\')');
 		$('.title').html(spells[$(this).attr('id')].title);
 		$('.description').html(spells[$(this).attr('id')].description);
 		$('.reallife').html(spells[$(this).attr('id')].reallife);
